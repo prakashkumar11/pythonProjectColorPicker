b2776c4b6be4bd37e36e7ec6d3e02e5dd2eca43e
Prakash Kumar <prakashsharma72092@gmail.com>
2024-05-06 11:02:19 +0530

    color picker project in python

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,24 @@
+# color2
+# colors2=Canvas(frame,bg="#fff",width=150,height=265,bd=0)
+# colors2.place(x=180,y=90)
+
+# id6 = colors2.create_rectangle((10,10,50,50),fill="#b8255f")
+# id7 = colors2.create_rectangle((10,50,50,100),fill="#db4035")
+# id8 = colors2.create_rectangle((10,100,50,150),fill="#ff9933")
+# id9 = colors2.create_rectangle((10,150,50,200),fill="#fad000")
+# id10 = colors2.create_rectangle((10,200,50,250),fill="#afb83b")
+
+# hex6=Label(colors2,text="#b8255f",fg="#000",font="arial 12 bold",bg="white")
+# hex6.place(x=60,y=15)
+
+# hex7=Label(colors2,text="#db4035",fg="#000",font="arial 12 bold",bg="white")
+# hex7.place(x=60,y=65)
+
+# hex8=Label(colors2,text="#ff9933",fg="#000",font="arial 12 bold",bg="white")
+# hex8.place(x=60,y=115)
+
+# hex9=Label(colors2,text="#fad000",fg="#000",font="arial 12 bold",bg="white")
+# hex9.place(x=60,y=165)
+
+# hex10=Label(colors2,text="#afb83b",fg="#000",font="arial 12 bold",bg="white")
+# hex10.place(x=60,y=215)
